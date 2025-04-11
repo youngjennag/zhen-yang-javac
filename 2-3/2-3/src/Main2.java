be33af1c2f3d4f512709d3b12029e5b8a6780012
youngjennag <chiny8174@gmail.com>
2025-04-11 14:34:19 +0900

    Delete 2-3/2-3 directory

--- 2-3/2-3/src/Main2.java
+++
@@ -1,6 +0,0 @@
-public class Main2 {
-    public static void main(String[] args){
-        System.out.println("今日の天気: 晴れ");
-        System.out.println("気温: 23.5℃");
-    }
-}
--- 2-3/2-3/src/Main5.java
+++
@@ -1,8 +0,0 @@
-public class Main5 {
-    public static void main(String[] args){
-        // mathScoreという変数を宣言し、85で初期化する
-        int mathScore = 85;
-        // mathScoreを出力する
-        System.out.println("数学の点数: "+ mathScore);
-    }
-}
